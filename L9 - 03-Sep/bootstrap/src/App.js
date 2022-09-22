b5f2b833012f4dff4913a3eb7941c66da1ce0f48
vishal Raj <rvishl05@gmail.com>
2022-09-22 22:34:59 +0530

    L9 3-sep by a sir

---
+++ L9 - 03-Sep/bootstrap/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
