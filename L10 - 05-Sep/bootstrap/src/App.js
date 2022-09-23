76d5de95796ed106bf35acff87c2141426189d28
vishal Raj <rvishl05@gmail.com>
2022-09-23 17:30:40 +0530

    L10 5-sep doubt class

---
+++ L10 - 05-Sep/bootstrap/src/App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import NavScrollExample from './components/NavScrollExample';
+import Abcd from './components/UncontrolledExample.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App() {
+  return (
+    <div>
+      <NavScrollExample />
+      <hr />
+      <Abcd />
+    </div>
+  );
+}
+
+export default App;
