4e7a4d07340107944aabf3cc5310ab7c624ddb2d
vishal Raj <rvishl05@gmail.com>
2022-10-13 17:13:22 +0530

    update

--- L17 - 14-Sep/emoji/src/components/Nav.js
+++ L17 - 14-Sep/emoji/src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
             <div className="img"><img src={happy} alt="logo" /></div>
             <div className="nav-header">
                 <h2>Emoji</h2>
-                <a href="https://github.com/rvishl/emoji-search---Project.git" target="_blank">Source</a>
+                <a href="https://github.com/rvishl/emoji-search---Project.git" target="_blank" rel="noreferrer">Source</a>
             </div>
             
         </nav>
